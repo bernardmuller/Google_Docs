610262441f8b4e70a085c356e81607243d5207f3
Bernard Muller <b.mullerjnr@gmail.com>
2021-07-21 21:27:30 +0200

    'New_Document_Section'

--- google-docs-clone/components/Header/Header.jsx
+++ google-docs-clone/components/Header/Header.jsx
@@ -1,17 +1,55 @@
 import React from 'react'
 import Button from "@material-tailwind/react/Button";
 import Icon from "@material-tailwind/react/Icon";
-import HeaderLeft from './HeaderLeft';
-import HeaderRight from './HeaderRight';
-import HeaderMiddle from './HeaderMiddle';
 
 function Header() {
     return (
         <div className='sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white w-auto'>
-            <HeaderLeft />
-            <HeaderMiddle />
-            <HeaderRight />
+            <div className='flex flex-inline items-center '>
+                <Button
+                    color="gray"
+                    buttonType="outline"
+                    rounded={true}
+                    iconOnly={true}
+                    ripple="dark"
+                    className="h-20 w-20 border-0"
+                >
+                    <Icon name="menu" size="3x1" />
+                </Button>
+                <Icon 
+                    name='description' 
+                    size='5x1'
+                    color='blue'
+                />
+                <h1 className='ml-2 text-gray-700 text-2x1'>
+                    Docs
+                </h1>
+            </div>
+            <div className='mx-5 md:mx-20 flex flex-grow items-center px-5 py-2 bg-gray-100 text-gray-600 rounded focus-within:text-gray-600 focus-within:shadow-md'>
+                <Icon name='search' size='3x1' color='darkgray' /> 
+                <input type="text" placeholder="Search" className='flex-grow px-5 text-sm bg-transparent outline-none' />
+            </div>
+            <div className='flex flex-inline items-center '>
+                <Button 
+                    color='gray'
+                    buttonType='outline'
+                    rounded={true}
+                    iconOnly={true}
+                    ripple='dark'
+                    className='ml-5 md:ml-20 h-20 w-20 border-0'>
+                    
+                    <Icon name='apps' size='3x1' color='gray' />
 
+                </Button>
+
+                <img
+                    loading='lazy'
+                    // onClick={signOut}
+                    className='cursor-pointer h-12 w-12 rounded-full ml-2'
+                    src='https://avatars.githubusercontent.com/u/61446115?v=4'
+                    alt=''
+                />
+            </div>            
         </div>
     )
 };
